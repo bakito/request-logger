c7db8ec4b30605da1117fa05e456b46b1f9d7569
bakito <github@bakito.ch>
2018-11-08 23:21:43 +0100

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM golang:1.11 as builder
+
+WORKDIR /go/src/github.com/bakito/request-logger
+
+COPY ./* /go/src/github.com/bakito/request-logger/
+
+RUN apt-get update && apt-get install -y xz-utils && \
+  curl -s -L  https://github.com/upx/upx/releases/download/v3.95/upx-3.95-amd64_linux.tar.xz -o upx.tar.xz && \
+  tar -xJf upx.tar.xz --strip=1 upx-3.95-amd64_linux/upx
+
+
+RUN GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags="-w -s" -o request-logger && \
+  ./upx request-logger
+
+# application image
+
+FROM scratch
+
+LABEL maintainer="bakito <github@bakito.ch>"
+EXPOSE 8080
+USER 1001
+ENTRYPOINT ["/go/bin/request-logger"]
+
+COPY --from=builder /go/src/github.com/bakito/request-logger/request-logger /go/bin/request-logger
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"sync/atomic"
 
 	"github.com/gorilla/mux"
 )
@@ -13,22 +14,24 @@ const (
 	port int = 8080
 )
 
+var counter uint64
+
 var handler = func() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
+		atomic.AddUint64(&counter, 1)
 		dump, err := httputil.DumpRequest(r, true)
 		if err != nil {
 			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 			return
 		}
-		log.Printf("Request:\n%s\n\n", (dump))
+		log.Printf("Request %v:\n%s\n\n", counter, dump)
 	})
 }
 
 func main() {
+	atomic.AddUint64(&counter, 0)
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(handler())
-
 	log.Printf("Running on port %v ...\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%v", port), r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), r))
 }

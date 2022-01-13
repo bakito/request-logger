fca9f15966c93a67b707778735909e2454485bd3
bakito <github@bakito.ch>
2022-01-13 20:43:41 +0100

    use crypto rand

--- pkg/handler/random.go
+++ pkg/handler/random.go
@@ -1,8 +1,9 @@
 package handler
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"math/big"
 	"net/http"
 	"strconv"
 	"time"
@@ -16,9 +17,13 @@ func RandomCode(w http.ResponseWriter, r *http.Request) {
 	code, _ := strconv.Atoi(mux.Vars(r)["code"])
 	perc, _ := strconv.ParseFloat(mux.Vars(r)["perc"], 64)
 
-	random := rand.Float64() // #nosec G404 weak random number generator is ok
+	random, err := rand.Int(rand.Reader, big.NewInt(100))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	delta := random - perc
+	delta := float64(random.Int64())/100. - perc
 	if delta <= 0.0 {
 		w.WriteHeader(code)
 	} else {
@@ -30,15 +35,19 @@ func RandomCode(w http.ResponseWriter, r *http.Request) {
 
 // RandomSleep sleep randomly
 func RandomSleep(w http.ResponseWriter, r *http.Request) {
-	sleep, err := strconv.Atoi(mux.Vars(r)["sleep"])
+	sleep, err := strconv.ParseInt(mux.Vars(r)["sleep"], 0, 64)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	random := rand.Intn(sleep) // #nosec G404 weak random number generator is ok
+	random, err := rand.Int(rand.Reader, big.NewInt(sleep))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	fmt.Printf("%v: %v Sleep: %dms\n", common.HeaderReqNo, w.Header()[common.HeaderReqNo][0], random)
-	time.Sleep(time.Duration(random) * time.Millisecond)
+	time.Sleep(time.Duration(random.Int64()) * time.Millisecond)
 	fmt.Printf("%v: %v Sleep: done\n", common.HeaderReqNo, w.Header()[common.HeaderReqNo][0])
 }
